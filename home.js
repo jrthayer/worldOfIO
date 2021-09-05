4f44a4a98c5edec0d72294e55371981e56432bc9
James Thayer <jrthayer@users.noreply.github.com>
2021-09-05 04:04:49 -0700

    Add initial files

---
+++ home.js
@@ -0,0 +1,268 @@
+let utcTimes = [
+    [],
+    [[1380, "The Nova Hellscape"]],
+    [[1200, "Mainframe of Atlas"]],
+    [[1440, "Goblins of Io"]],
+    [[1380, "Spire of Euclid"]],
+    [[1380, "Shadow of Asmodia"]],
+    [[1200, "Miss Demeanor"]],
+];
+
+let carouselWidth = 0;
+let dayWidth = 0;
+let dayLength = 0;
+
+//------------//------------//------------
+//  1.2 Initialize Components
+//------------//------------//------------
+
+import * as schedule from "./schedule.js";
+import * as countdown from "./countdown.js";
+
+let scheduleElement = document.querySelector("#schedule-section");
+let countdownElement = document.querySelector("#countdown-container");
+
+let convertedWeek = schedule.convertWeek(utcTimes);
+let scheduleHTML = schedule.createSchedule(convertedWeek);
+let leftIcon = document.createElement("i");
+leftIcon.classList.add("fas", "fa-angle-double-left");
+let rightIcon = document.createElement("i");
+rightIcon.classList.add("fas", "fa-angle-double-right");
+
+let carouselHtml = scheduleHTML.querySelector(".schedule-carousel-days");
+let leftBtn = scheduleHTML.querySelector("button:first-of-type");
+leftBtn.appendChild(leftIcon);
+leftBtn.addEventListener("click", () => carouselScroll(-1, carouselHtml));
+let rightBtn = scheduleHTML.querySelector("button:last-of-type");
+rightBtn.appendChild(rightIcon);
+rightBtn.addEventListener("click", () => carouselScroll(1, carouselHtml));
+scheduleElement.appendChild(scheduleHTML);
+
+setNextShow();
+
+function setNextShow() {
+    let next = nextShow(convertedWeek);
+    let showDay = next[0];
+    let showTime = convertedWeek[showDay][next[1]][0];
+    let showName = convertedWeek[showDay][next[1]][1];
+    let daysTill = daysTillShow(showDay);
+
+    let showNameFormatted = showName.replace(/\s/g, "");
+    showNameFormatted = showNameFormatted.replace("'", "");
+    showNameFormatted =
+        showNameFormatted[0].toLowerCase() + showNameFormatted.substring(1);
+    console.log(showNameFormatted);
+    let showImg = countdownElement.querySelector("video");
+    showImg.setAttribute("src", `videos/${showNameFormatted}.mp4`);
+    console.log(showImg);
+    let countdownHTML = countdown.create(showTime, showName, daysTill);
+    countdownElement
+        .querySelector(".countdown-data")
+        .appendChild(countdownHTML);
+
+    let intervalID = window.setInterval(() => {
+        if (countdown.data.finished) {
+            document.querySelector("#countdown").remove();
+            countdown.data.finished = false;
+            clearInterval(intervalID);
+            setNextShow();
+        }
+    }, 1000);
+}
+
+//------------//------------//------------
+//  2.1 Next Show
+//------------//------------//------------
+
+//Info: determines the next show on the schedule
+//------------
+//Return:
+//  +an array with:
+//      +index 0: int value of the day of the show;
+//      +index 1: index of show that day;
+function nextShow(schedule) {
+    let time = new Date();
+    let day = time.getDay();
+    let minutes = time.getMinutes() + time.getHours() * 60;
+
+    let loopNum = 0;
+    let showDay = 0;
+    let showInstance = 0;
+    let found = false;
+
+    while (found !== true) {
+        let curDay = day + loopNum;
+
+        //accounts for being greater than sun === 6
+        if (curDay > 6) curDay = curDay - 7;
+
+        //have to check all shows of current day
+        if (loopNum === 0) {
+            for (let x = 0; x < schedule[curDay].length; x++) {
+                //+1 ensures that next show runs at 0
+                if (minutes + 1 < schedule[curDay][x][0]) {
+                    showDay = curDay;
+                    showInstance = x;
+
+                    found = true;
+                    break;
+                }
+            }
+        } else {
+            //if next show is on another day it will be the first show of that day
+            if (schedule[curDay].length > 0) {
+                showDay = curDay;
+                showInstance = 0;
+
+                found = true;
+                break;
+            }
+        }
+
+        loopNum++;
+        if (loopNum >= 7) break;
+    }
+
+    //found no shows, only happens when there is only one show and it already occured today
+    if (found === false) {
+        showDay = day;
+        showInstance = 0;
+    }
+
+    return [showDay, showInstance];
+}
+
+//Info: determines days till weekly event
+//------------
+//Parameters:
+//  +eventDay: day of the event
+//------------
+//Return:
+//  +an int that is the days till the event
+function daysTillShow(eventDay) {
+    let date = new Date();
+    let curDay = date.getDay();
+
+    // days between now and next show
+    let daysBetween = 0;
+    if (curDay > eventDay) {
+        daysBetween = 7 - curDay + eventDay;
+    } else {
+        daysBetween = eventDay - curDay;
+    }
+
+    return daysBetween;
+}
+
+const slider = document.querySelector(".schedule-carousel-days");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    console.log("mouse down");
+});
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(slider.scrollLeft);
+});
+
+//carousel
+function carouselScroll(value, htmlElement) {
+    let curX = htmlElement.scrollLeft;
+    console.log(`curX: ${curX}, dayLength: ${dayLength}`);
+    let curShow = Math.floor(curX / dayLength);
+    console.log(dayLength);
+    let scrollValue = 0;
+    if (curX % dayLength !== 0 && value === -1) {
+        scrollValue = curShow * dayLength;
+    } else {
+        scrollValue = (curShow + value) * dayLength;
+    }
+    console.log(
+        `scrollValue: ${scrollValue}, curShow:${curShow}, value: ${value}, dayLength:${dayLength}`
+    );
+
+    if (curX % dayLength !== 0) {
+        // scrollValue += scrollValue + value * (curX % dayLength);
+        // console.log(curX % dayLength);
+    }
+
+    htmlElement.scrollTo({ top: 0, left: scrollValue, behavior: "smooth" });
+}
+
+//
+let videoBanners = document.querySelectorAll(".fullscreenAsset");
+let navBarHeight = getComputedStyle(document.documentElement).getPropertyValue(
+    "--navHeight"
+);
+
+navBarHeight = parseInt(navBarHeight);
+
+// resize
+function resizeFunctions() {
+    videoBannerCheck();
+    carouselCheck(carouselHtml);
+}
+
+function videoBannerCheck() {
+    let intViewportHeight = window.innerHeight;
+    let intViewportWidth = document.body.clientWidth;
+
+    let style = getComputedStyle(videoBanners[0]);
+    let height = parseInt(style.height);
+    let width = parseInt(style.width);
+
+    for (let x = 0; x < videoBanners.length; x++) {
+        if (height <= intViewportHeight) {
+            videoBanners[x].classList.add("bannerHeight");
+        }
+
+        if (width < intViewportWidth) {
+            videoBanners[x].classList.remove("bannerHeight");
+        }
+    }
+}
+
+function carouselCheck(htmlElement) {
+    let scrollWidth = htmlElement.scrollWidth;
+    carouselWidth = parseInt(scrollWidth);
+
+    let shownWidth = getComputedStyle(htmlElement).getPropertyValue("width");
+    shownWidth = parseInt(shownWidth);
+
+    let dayElement = htmlElement.querySelector(".row");
+    dayWidth = getComputedStyle(dayElement).getPropertyValue("width");
+    dayWidth = parseInt(dayWidth);
+    console.log(dayWidth);
+
+    let totalDays = Math.floor(scrollWidth / dayWidth);
+    let shownDays = Math.floor(shownWidth / dayWidth);
+    console.log(totalDays);
+    console.log(shownDays);
+
+    // dayLength = (scrollWidth - shownWidth) / (totalDays - shownDays);
+    // dayLength = Math.floor(dayLength);
+    dayLength = shownWidth;
+    // console.log(`${scrollWidth - shownWidth} days: ${totalDays - shownDays}`);
+    // console.log(dayLength);
+}
+
+resizeFunctions();
+window.addEventListener("resize", resizeFunctions);
---
+++ schedule.js
@@ -0,0 +1,259 @@
+//------------//------------//------------//------------//------------
+//      TABLE OF CONTENTS
+//------------//------------//------------//------------//------------
+//      1. Features
+//          1.1 Time Conversion
+//          1.2 Type Conversion
+//          1.3 Html Generators
+//------------//------------//------------//------------//------------
+
+//------------//------------//------------//------------
+//      1. Features
+//------------//------------//------------//------------
+
+//------------//------------//------------
+//  1.1 Time Conversion
+//------------//------------//------------
+
+//Info: Converts a week of shows into local timezone(in minutes)
+//------------
+//Parameters:
+//  +showArray: an array of arrays.
+//      +length of 7(one index for each day of week)
+//      +each show is a 2 index array:
+//          +index 0: show time in utc
+//          +index 1: show name
+//Return:
+//  +an array of arrays:
+//      +length of 7(one index for each day of week)
+//      +each show is a 2 index array:
+//          +index 0: show time in utc
+//          +index 1: show name
+export function convertWeek(showArray) {
+    let convertedArray = [[], [], [], [], [], [], []];
+
+    //loop through each day of week
+    for (let x = 0; x < 7; x++) {
+        let day = showArray[x];
+
+        //loop through each show of day
+        for (let y = 0; y < day.length; y++) {
+            let showName = day[y][1];
+            let convertData = convertDay(day[y][0], x);
+
+            let showTime = convertData[0];
+            let showDay = convertData[1];
+
+            //insert into array
+            let showInstance = 0;
+            for (let z = 0; z < convertedArray[showDay].length; z++) {
+                let comparedDay = convertedArray[showDay][z];
+
+                if (comparedDay[0] < showTime) {
+                    showInstance = z + 1;
+                }
+            }
+
+            convertedArray[showDay].splice(showInstance, 0, [
+                showTime,
+                showName,
+            ]);
+        }
+    }
+
+    return convertedArray;
+}
+
+//Info: Converts shows into local timezone(in minutes)
+//------------
+//Parameters:
+//  +index = represents day of show
+//  +utcTime = utc time in minutes being converted
+//Return:
+//  +an array with:
+//      +index 0: local time in minutes;
+//      +index 1: local day as an int 0 === sunday;
+export function convertDay(utcTime, utcDay) {
+    let date = new Date();
+    let offset = date.getTimezoneOffset();
+    offset = offset * -1;
+
+    let localTime = utcTime + offset;
+
+    let dayOffset = 0;
+
+    if (localTime < 0) {
+        localTime = 60 * 24 + localTime;
+        dayOffset = -1;
+    } else if (localTime > 1440) {
+        localTime = localTime - 1440;
+        dayOffset = 1;
+    }
+
+    let day = utcDay + dayOffset;
+
+    if (day > 6) day = 0;
+    if (day < 0) day = 6;
+
+    let convertedTime = [localTime, day];
+    return convertedTime;
+}
+
+//------------//------------//------------
+//  1.2 Type Conversion
+//------------//------------//------------
+
+//Info: Converts minutes into am/pm format
+//------------
+//Parameters:
+//  +minutes = int representing minutes
+//Return:
+//  +converts minutes to am/pm
+export function convertToTime(minutes) {
+    var hours = Math.floor(minutes / 60);
+    var min = minutes - hours * 60;
+
+    if (min < 10) {
+        min = "0" + min;
+    }
+
+    var ampm = "am";
+    if (hours > 12) {
+        hours = hours - 12;
+        ampm = "pm";
+    } else if (hours == 12) {
+        ampm = "pm";
+    }
+
+    if (hours == 0) {
+        hours = 12;
+    }
+
+    var time;
+    if (min == "00") {
+        time = hours + ampm;
+    } else {
+        time = hours + ":" + min + ampm;
+    }
+
+    return time;
+}
+
+//Info: Converts int of day into string
+//------------//------------
+//Parameters:
+//  +index = int representing day
+//------------
+//Return:
+//  +day in string format
+export function convertToDay(index) {
+    var day;
+
+    if (index == 0) {
+        day = "Sun";
+    } else if (index == 1) {
+        day = "Mon";
+    } else if (index == 2) {
+        day = "Tues";
+    } else if (index == 3) {
+        day = "Wed";
+    } else if (index == 4) {
+        day = "Thurs";
+    } else if (index == 5) {
+        day = "Fri";
+    } else if (index == 6) {
+        day = "Sat";
+    } else {
+        day = "HOW";
+    }
+
+    return day;
+}
+
+//------------//------------//------------
+//  1.3 Html Generators
+//------------//------------//------------
+
+//Info: Dynamically creates html for the show schedule
+//------------
+//Parameters:
+//  +showArray: an array of arrays.
+//      +length of 7(one index for each day of week)
+//      +each show is a 2 index array:
+//          +index 0: show time in utc
+//          +index 1: show name
+export function createSchedule(showArray) {
+    let container = document.createElement("div");
+    container.id = "schedule-container";
+
+    let header = document.createElement("h1");
+    header.innerHTML = "Schedule";
+    container.appendChild(header);
+    let dayContainerExtra = document.createElement("div");
+    let dayContainer = document.createElement("div");
+    dayContainerExtra.classList.add("schedule-carousel");
+    dayContainer.classList.add("schedule-carousel-days");
+    let leftBtn = document.createElement("button");
+    let rightBtn = document.createElement("button");
+    dayContainerExtra.appendChild(leftBtn);
+    dayContainerExtra.appendChild(rightBtn);
+    dayContainerExtra.appendChild(dayContainer);
+
+    for (let day = 0; day < 7; day++) {
+        if (showArray[day].length >= 1) {
+            let daySchedule = createDaySchedule(day, showArray[day]);
+            dayContainer.appendChild(daySchedule);
+        }
+    }
+
+    container.appendChild(dayContainerExtra);
+
+    return container;
+}
+
+//Info: Creates html element of individual day
+//------------//------------
+//Parameters:
+//  +day = int representing day being created
+//  +daySchedule = array of shows for that day
+//      +each show is a 2 index array:
+//          +index 0: show time in utc
+//          +index 1: show name
+//------------
+//Return:
+//  +html element of day created
+export function createDaySchedule(day, daySchedule) {
+    let dayElement = document.createElement("div");
+    dayElement.classList.add("row");
+    let label = document.createElement("h3");
+    label.classList.add("label");
+    label.innerHTML = convertToDay(day);
+    let data = document.createElement("div");
+    data.classList.add("data");
+    dayElement.appendChild(label);
+    dayElement.appendChild(data);
+
+    for (let x = 0; x < daySchedule.length; x++) {
+        let showImg = document.createElement("img");
+
+        let showString = daySchedule[x][1];
+        let showNameFormatted = showString.replace(/\s/g, "");
+        showNameFormatted = showNameFormatted.replace("'", "");
+        showNameFormatted =
+            showNameFormatted[0].toLowerCase() + showNameFormatted.substring(1);
+        showImg.src = `/images/${showNameFormatted}.png`;
+        data.appendChild(showImg);
+
+        let show = document.createElement("div");
+        show.classList.add("gameInfo");
+        let showName = document.createElement("div");
+        showName.innerHTML = showString;
+        let showTime = document.createElement("div");
+        showTime.innerHTML = convertToTime(daySchedule[x][0]);
+        show.appendChild(showName);
+        show.appendChild(showTime);
+        data.appendChild(show);
+    }
+
+    return dayElement;
+}
